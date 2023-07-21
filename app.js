b6b1cf71939020393f5b45b5b832bc52727d88b5
gursewakkular72 <gursewakkular72@gmail.com>
2023-07-21 09:57:48 -0700

    after adding some basic route functionality

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+//set up middle ware
+app.use(express.json());
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('hello from the server');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+// console.log(tours);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log('ran');
+  // get the the body
+  const totalTours = tours.length - 1;
+  const newTourId = totalTours + 1;
+  const newTour = Object.assign({ id: newTourId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err)
+        res.status(500).send('Can not save the tours. Something went wrong :(');
+      res.status(201).json(tours);
+    }
+  );
 });
 
 app.listen(port, () => {
